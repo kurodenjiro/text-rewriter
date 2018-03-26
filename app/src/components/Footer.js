9627e5b0ad551ced1c1408157a9ae777c737163b
Kevin Danikowski <kdanikowski@outlook.com>
2018-03-25 22:04:19 -0500

    fixed some https links

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-const Footer = () =>
-    <div className='footer flex justify-end items-center'>
-        <a className='mr4' href='https://smodin.me/home'>Main Site</a>
-        <Link to='/api' className='mr3'>API</Link>
-    </div>
+class Footer extends Component {
+    render(){
+        return(
+            <div className='footer flex justify-end items-center'>
+                <a className='mr4' href='https://smodin.me/home'>Main Site</a>
+                <Link to='/api' className='mr3'>API</Link>
+            </div>
+        )
+    }
+}
 
 export default Footer;
 
