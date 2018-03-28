af8c9fcb0f168cdc6436fc09cf2e2bf3179458ea
Kevin Danikowski <kevin.danikowski@webtomed.com>
2018-03-28 15:46:09 -0500

    added .envsample, added build dir, added api testing

--- api/test/helpers.js
+++ api/test/helpers.js
@@ -1,5 +1,6 @@
 import { expect, assert } from 'chai';
 import { sinon, spy } from 'sinon';
+import 'babel-polyfill'; //imported because issue https://github.com/babel/babel/issues/5085
 
 global.expect = expect;
 global.assert = assert;
