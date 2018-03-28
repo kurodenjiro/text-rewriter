0a8e7609ded60bb8efb20d74cca76529d7297731
Kevin Danikowski <kevin.danikowski@webtomed.com>
2018-03-28 16:55:29 -0500

    added server tests

--- api/test/helpers.js
+++ api/test/helpers.js
@@ -1,9 +1,12 @@
-import { expect, assert } from 'chai';
+import Chai from 'chai'
+import ChaiHttp from 'chai-http'
+Chai.use(ChaiHttp)
 import { sinon, spy } from 'sinon';
 import 'babel-polyfill'; //imported because issue https://github.com/babel/babel/issues/5085
 
-global.expect = expect;
-global.assert = assert;
+global.expect = Chai.expect;
+global.request = Chai.request;
+global.assert = Chai.assert;
 global.sinon = sinon;
 global.spy = spy;
 
