000afa732161f4fe1b93ea44e56160eab9e2ca64
Kevin Danikowski <kevin.danikowski@webtomed.com>
2018-04-20 16:02:37 -0500

    fixing testing environment

--- app/test/Home.spec.js
+++ app/test/Home.spec.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { MockedProvider } from "react-apollo/test-utils";
-import {LANGUAGE_COMBINATIONS_QUERY} from '../src/graphql'
+import {LANGUAGE_COMBINATIONS_QUERY} from '../src/graphql/queries'
 import Home from '../src/components/Home';
 
-const defaultProps = {
-    text: 'this is my child ;)'
-}
+const defaultProps = {}
 
 describe('Home', () => {
     it('should return true', ()=>{
         expect(true).to.equal(true);
     })
     const mockedData = {
-        languageCombinations: [
-            { __typename: 'languageCombination', id: '1', processingLanguages: ['es'], language: 'en', avgRating: '3.2', ratingCount: 6, ratings: []}
+        LanguageCombinations: [
+            { __typename: 'LanguageCombination', id: '1', processingLanguages: ['es'], language: 'en', avgRating: '3.2', ratingCount: 6, ratings: []}
         ]
     }
     const query = LANGUAGE_COMBINATIONS_QUERY//addTypenameToDocument(LANGUAGE_COMBINATIONS_QUERY)
@@ -24,22 +22,15 @@ describe('Home', () => {
             ]}>
             <Home/>
         </MockedProvider>);
-    const mountedWrapper = mount(
-        <MockedProvider mocks={[
-            { request: { query, variables }, result: { data: mockedData }}
-        ]}>
-            <Home/>
-        </MockedProvider>)
-    // const renderedWrapper = mount(<Child/>)
     it('mockedprovider to have Home component', () => {
         expect(wrapper.contains(<Home />)).to.equal(true);
     });
     it('Rewrite btn to exist and work', () => {
-        const rewriteBtn = mountedWrapper.find('.btn_rewrite')
+        const rewriteBtn = wrapper.find('.btn_rewrite')
         expect(rewriteBtn).to.have.length(1);
-        mountedWrapper.find('textarea').get(0).value('this should be rewritten')
+        wrapper.find('textarea').get(0).value('this should be rewritten')
         rewriteBtn.simulate('click')
-        expect(mountedWrapper.find('rewrite-result')).to.have.value('string')
+        expect(wrapper.find('rewrite-result')).to.have.value('string')
     });
 
 })
